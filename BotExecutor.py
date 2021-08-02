d4abb164bdb7d21b5444dc6b88863bd472fad651
Peter Antonaros <46857643+peterantonarosjr@users.noreply.github.com>
2021-08-02 19:47:09 -0400

    Login/Authentication

--- BotExecutor.py
+++ BotExecutor.py
@@ -1,21 +1,36 @@
 import robin_stocks.robinhood as r
 import robin_stocks.gemini as c
 import pyotp
-import time
-from datetime import datetime
+from Utilities import *
 
-credentialFile = open('D:\\Pycharm-Workspace\\CryptoTradeBot\\RH.txt').read().splitlines()
-EMAIL = credentialFile[0]
-PASSWORD = credentialFile[1]
-KEY = credentialFile[2]
-CODE = credentialFile[3]
-totp = pyotp.TOTP(KEY).now()
-try:
-    r.login(EMAIL,PASSWORD,mfa_code=CODE)
-    c.authentication.login(KEY, CODE)
-    c.authentication.heartbeat(jsonify=None)
-    print("Successful Login/Authentication")
-except:
-    print("Failed Login/Authentication")
+def login(numOfDays):
+    seconds = 60
+    secPerMinute = seconds*60
+    secPerHour = secPerMinute*60
+    secPerDay = secPerHour*24
+    loginDuration = numOfDays*secPerDay
 
+    credentialFile = open('D:\\Pycharm-Workspace\\CryptoTradeBot\\RH.txt').read().splitlines()
+    EMAIL = credentialFile[0]
+    PASSWORD = credentialFile[1]
+    KEY = credentialFile[2]
+    CODE = credentialFile[3]
+    totp = pyotp.TOTP(KEY).now()
+    try:
+        r.login(EMAIL, PASSWORD, mfa_code=totp, expiresIn=loginDuration,store_session=False)
+        c.authentication.login(KEY, CODE)
+        c.authentication.heartbeat(jsonify=None)
+        print("Successful Login/Authentication")
+    except:
+        print("Failed Login/Authentication")
+
+def logout():
+    r.logout()
+
+def main():
+    login(1)
+    logout()
+
+if __name__ == "__main__":
+    main()
 
