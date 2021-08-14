d876784ef3d5fb6f1e7ded5a4ce00003958ef540
Peter Antonaros <46857643+peterantonarosjr@users.noreply.github.com>
2021-08-14 19:50:45 -0400

    main trading loop

--- BotExecutor.py
+++ BotExecutor.py
@@ -1,8 +1,13 @@
-import robin_stocks.robinhood as r
-import robin_stocks.gemini as c
-import pyotp
+import Utilities
+from TradingStrategies import GoldenDeathCross
+from TradingStrategies import RSIDivergence
+from TradingStrategies import Scalping
 from Utilities import *
+import pyotp
+import time
+import datetime
 
+#User authentication and login for a specified number of days
 def login(numOfDays):
     seconds = 60
     secPerMinute = seconds*60
@@ -24,19 +29,41 @@ def login(numOfDays):
     except:
         print("Failed Login/Authentication")
 
+#User logout
 def logout():
     r.logout()
 
-def main():
-    login(1)
-    cash = getLiquidity()
-    equity = getCryptoEquity()
-    print("Account liquidity: $"+str(cash))
-    print("Account equity: $"+str(equity))
-    print(buildDatabase("eth","day","week"))
+#Takes in loginDuration (# of days) and updateFrequency (# of minutes)
+def main(loginDuration,updateFrequency):
+    login(loginDuration)
+    loginTime = datetime.datetime.now()
+    logoutTime = loginTime + datetime.timedelta(loginDuration)
+    currentTime = datetime.datetime.now()
+
+    cryptoTickerList = cryptosToTrade()
+
+    crossStrategy = GoldenDeathCross.CrossStrategy(cryptoTickerList)
+
+    while currentTime<logoutTime:
+        cash = getLiquidity()
+        equity = getCryptoEquity()
+
+        print(getCryptoPrices(cryptoTickerList))
+        print(crossStrategy.getShortMovingAverage()[1])
+        print(crossStrategy.getLongMovingAverage()[1])
+
+
+
+        #MAIN TRADING STUFF GOES HERE
+
+        time.sleep(updateFrequency)
+        crossStrategy.update()
+        currentTime = datetime.datetime.now()
 
     logout()
 
+
 if __name__ == "__main__":
-    main()
+    #Input # days login, # seconds update delta
+    main(1,5)
 
